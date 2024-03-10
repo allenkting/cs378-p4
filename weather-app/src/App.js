dc2c623175bd9fa0f8f5a107b540872e777d9b46
Allen Ting <allenkting1@gmail.com>
2024-03-09 23:01:33 -0600

    buttons work

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,38 +1,92 @@
 import './App.css';
 import Fetch from './components/Fetch'
+import { useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// async function getLatLong(cityName) {
+//   try {
+//       const response = await fetch('https://geocoding-api.open-meteo.com/v1/search?name=' + cityName + '&count=1&language=en&format=json');
+//       const json = await response.json();
+//       console.log(json); 
+//       return json;
+//   }
+//   catch (e) {
+//       console.log("Error: " + e)
+//       return {};
+//   }
+// }
+
+// async function asyncCaller(cityName) {
+//   const result = await getLatLong(cityName);
+//   return result;
+// }
+
+const initialCities = [
+  {
+    id:1,
+    name:"Austin",
+    latitude: 30.26715,
+    longitude: -97.74306,
+  },
+  {
+    id:2,
+    name:"Dallas",
+    latitude: 32.78306,
+    longitude: -96.80667,
+  },
+  {
+    id:3,
+    name:"Houston",
+    latitude: 29.76328,
+    longitude: -95.36327,
+  }
+]
 
 function App() {
-  // let apiUrl = "https://api.open-meteo.com/v1/forecast?temperature_unit=fahrenheit"
-  // const getLatLong = (cityName) => {
-    
-  // }
+  // const result = asyncCaller("Austin");
+  const [coords, setCoords] = useState({"latitude": initialCities[0].latitude,
+  "longitude": initialCities[0].longitude});
+
+  useEffect(() => {
+    setCoords(coords);
+  }, [coords])
 
   return (
     <div class="container">
       
       <div class="row align-items-center cities">
         <div class="col">
-          <button type="button" class="but">
+          <button type="button" class="but" onClick={() => {
+            setCoords({"latitude": initialCities[0].latitude,
+            "longitude": initialCities[0].longitude})
+          }}>
             Austin
           </button>
         </div>
         <div class="col">
-          <button type="button" class="but">
+          <button type="button" class="but" onClick={() => {
+            setCoords({"latitude": initialCities[1].latitude,
+            "longitude": initialCities[1].longitude})
+          }}>
             Dallas
           </button>
         </div>
         <div class="col">
-          <button type="button" class="but">
+          <button type="button" class="but" onClick={() => {
+            setCoords({"latitude": initialCities[2].latitude,
+            "longitude": initialCities[2].longitude})
+          }}>
             Houston
           </button>
         </div>
       </div>
 
       <div class="row align-items-center text-input">
-        <div class="col-9 text-center">
-          <input name="cityInput" class="input"/>
+        <div class="col-4 text-center">
+          <input name="latInput" class="input"/>
+        </div>
+        <div class="col-4 text-center">
+          <input name="longInput" class="input"/>
         </div>
         <div class="col">
           <button type="button" class="but">
@@ -50,7 +104,7 @@ function App() {
                   <b>Temperature</b>
               </div>
           </div>
-        <Fetch cityName="Austin"/>
+        <Fetch lat={coords["latitude"]} long={coords["longitude"]}/>
       </div>
 
     </div>
--- weather-app/src/components/Fetch.js
+++ weather-app/src/components/Fetch.js
@@ -3,24 +3,8 @@ import { useState, useEffect } from 'react';
 import TempInfo from './TempInfo';
 
 
-const Fetch = ({cityName}) => {
-    
-    let geoUrl = "https://geocoding-api.open-meteo.com/v1/search?name=" + cityName + "&count=1&language=en&format=json";
-    const [info, setInfo] = useState({});
-    useEffect(() => {
-        fetch(geoUrl)
-        .then((res) => {return res.json()})
-        .then((data) => {
-            console.log(data);
-            setInfo(data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }, [geoUrl]);
+const Fetch = ({lat, long}) => {
 
-    let lat = 0//info.latitude;
-    let long = 0//info.longitude;
     let tempUrl = "https://api.open-meteo.com/v1/forecast?temperature_unit=fahrenheit" + 
                     "&latitude=" + lat + "&longitude=" + long + "&hourly=temperature_2m"
     const [temps, setTemps] = useState([]);
@@ -31,10 +15,11 @@ const Fetch = ({cityName}) => {
         .then((res) => {return res.json()})
         .then((data) => {
             console.log(data);
-            setTemps(data.hourly.temperature_2m.slice(0,11));
-            setTimes(data.hourly.time.slice(0,11));
+            setTemps(data.hourly.temperature_2m.slice(0,15));
+            setTimes(data.hourly.time.slice(0,15));
         })
         .catch((error) => {
+            alert("Could not find weather for those coordinates")
             console.log(error);
         });
     }, [tempUrl]);
